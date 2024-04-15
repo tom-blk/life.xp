9d680684451b0106f632eca1fb150e751b324a85
tom-blk <tom.bleek@proton.me>
2024-04-15 18:03:25 +0200

    Started woking on navbar

--- components/buttons/nav-button/NavButton.Component.tsx
+++ components/buttons/nav-button/NavButton.Component.tsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import { Pressable, StyleSheet, Text } from 'react-native'
 
 const NavButton = () => {
+  
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const onPress = () => {
+    setMenuIsOpen(!menuIsOpen);
+  }
+
   return (
-    <div style={styles.button}>
+    <div>
+      <Pressable onPress={onPress} style={styles.button}>
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <line x1="12" y1="5" x2="12" y2="19" />
             <line x1="5" y1="12" x2="19" y2="12" />
         </svg>
+      </Pressable>
+      <div>
+        <Text>Skills</Text>
+        <Text>Add Skill</Text>
+        <Text>Stats</Text>
+      </div>
     </div>
   )
 }
