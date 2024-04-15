02ecfd169cf3dcd7aae03bc2e120f3314e9316e2
tom-blk <tom.bleek@proton.me>
2024-04-15 18:32:55 +0200

    Converted App.js to App.tsx and added missing import

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import SkillList from './components/pages/skill-list/SkillList.Component';
+import AddSkill from './components/pages/add-skill/AddSkill.Component';
 
 const Drawer = createDrawerNavigator();
 
