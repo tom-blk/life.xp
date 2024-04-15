59ca59c9318f07d0fc6cd3155477e20537c3c33e
tom-blk <tom.bleek@proton.me>
2024-04-15 18:17:48 +0200

    Scrapped nav button and went for react-navigation instead

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import NavButton from './components/buttons/nav-button/NavButton.Component';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import SkillList from './components/pages/skill-list/SkillList.Component';
+
+const Drawer = createDrawerNavigator();
 
 const App = () => {
   return (
-    <View>
-      <SkillList />
-      <NavButton/>
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Skill List">
+        <Drawer.Screen name="Skill List" component={SkillList} />
+        <Drawer.Screen name="Add Skill" component={AddSkill} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
