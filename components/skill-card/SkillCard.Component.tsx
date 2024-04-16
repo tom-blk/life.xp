85fa9f183e8f2d3037f8f28852d04e259ce3aaba
tom-blk <tom.bleek@proton.me>
2024-04-16 21:44:07 +0200

    Added basic organizational style to skill card and took first steps towards proper navigation

--- components/skill-card/SkillCard.Component.tsx
+++ components/skill-card/SkillCard.Component.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
-import { Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text} from 'react-native';
 import { Skill } from '../../types/Skill';
+import { useNavigation } from '@react-navigation/native';
 
 const SkillCard = (props: Skill) => {
     const {skillName, level, secondsToNextLevel, secondsToLevelUp} = props;
+    const navigation = useNavigation();
+
+    const navigateToSkill = () => {
+
+    }
+
+    const navigateToTrainSkill = () => {
+
+    }
 
     return (
-        <View>
+        <Pressable style={styles.card} onPress={e =>navigateToSkill()}>
             <Text>{skillName}</Text>
             <Text>{level}</Text>
             <Text>{secondsToLevelUp}</Text>
             <Text>{secondsToNextLevel}</Text>
-        </View>
+            <Button title="Train" onPress={e => navigateToTrainSkill()}/>
+        </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 10,
+        borderRadius: 5
+    }
+})
 
 export default SkillCard
